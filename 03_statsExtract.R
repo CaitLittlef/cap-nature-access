4fd42f65b57c8677b3a12af59ea249aaa81caa26
CaitLittlef <caitlin.littlefield@uvm.edu>
2022-04-22 15:32:23 -0400

    zone select; add in additional code for odd-balls; cbind all

--- 03_statsExtract.R
+++ 03_statsExtract.R
@@ -4,8 +4,6 @@
 ############################################
 
 
-##--------------------------------------------
-
 ## REMEMBER THE UNIVERSE YOU'RE DEALING WITH!! 
 # Race alone - total pop
 # Income - households*
@@ -21,20 +19,26 @@
 # X # of households are low income. Rather, it will be X # of ppl
 # live in low-income tracts. (By contrast, we DO have # fams.)
 
-##--------------------------------------------
 
 
-## Select AOI
-d <- cr ; s <- txnm; loc = "Castner Range" ; print("Castner Range selected.")
-# d <- aka ; s <- nvca; loc = "Avi Kwa Ame" ; print("Avi Kwa Ame selected.")
 
+##---------------------------------------------------------------------
 
-## Buffer proposed nat'l monument at multiple intervals. Currently m but set to latlong
+## Create buffers around AOI and extract census tract data therein
+
 m_in_mi <- 1609
 
-b1 <- st_buffer(d, 10*m_in_mi) ; b1_size = "10 miles"
-b2 <- st_buffer(d, 25*m_in_mi) ; b2_size = "25 miles"
-b3 <- st_buffer(d, 50*m_in_mi) ; b3_size = "50 miles"
+# Select AOI and associated buffer sizes
+d <- cr ; s <- txnm; loc = "Castner Range" ; print("Castner Range selected.")
+b1 <- st_buffer(d, 10*m_in_mi) ; b1_size = "10mi"
+b2 <- st_buffer(d, 25*m_in_mi) ; b2_size = "25mi"
+b3 <- st_buffer(d, 50*m_in_mi) ; b3_size = "50mi"
+# 
+# d <- aka ; s <- nvca; loc = "AviKwaAme" ; print("Avi Kwa Ame selected.")
+# b1 <- st_buffer(d, 25*m_in_mi) ; b1_size = "25mi"
+# b2 <- st_buffer(d, 50*m_in_mi) ; b2_size = "50mi"
+# b3 <- st_buffer(d, 100*m_in_mi) ; b3_size = "100mi"
+
 
 plot(b3)
 plot(b2, add = TRUE)
@@ -42,15 +46,12 @@ plot(b1, add = TRUE)
 plot(d, add = TRUE)
 
 
-
 ## Find census tracts that intersect w each buffer.
-
 # 1st, ensure existing sqkm is correct to use to weight #ppl for partial tracts.
-area_sqkm <- terra::area(as_Spatial(s)) / 1000000
-s$area_km2[1:50] # already in shapefile
-area_sqkm[1:50]
-identical(round(txnm$area_km2,2), round(area_sqkm,2)) # TRUE
-s$area_sqkm <- NULL
+# area_sqkm <- terra::area(as_Spatial(s)) / 1000000
+# mean(abs(area_sqkm - s$area_km2)) ; max(abs(area_sqkm - s$area_km2)) 
+# identical(round(txnm$area_km2,4), round(area_sqkm,4)) # TRUE for CR, not quite AKA but close enough
+# area_sqkm <- NULL
 
 # Initial intersection threw this error
   # Error in s2_geography_from_wkb(x, oriented = oriented, check = check) : 
@@ -85,148 +86,163 @@ s_b3_df <- s_b3 ; s_b3_df$geometry <- NULL
 
 
 
-####################### DELETE ALL THIS
-# ## FOLD THIS ALL TOGETHER INTO JUST ONE TABLE OR DON'T EVEN WORRY AOBUT PULLING OUT VALS
-# 
-# ## Pull national and state-level hm and hme (human mod from energy) vals
-# # Tracts_ind represent total pop (per functions in util.R); use fam for children-defined tracts. 
-# attach(hm_natl)
-# (hm_natl_ind_mean <- hm_mean[status_group == "tracts_ind"])
-# (hme_natl_ind_mean <- hm_energy_mean[status_group == "tracts_ind"])
-# (hm_natl_fam_mean <- hm_mean[status_group == "tracts_fam"])
-# (hme_natl_fam_mean <- hm_energy_mean[status_group == "tracts_fam"])
-# detach(hm_natl)
-# 
-# # Pull state names to use as filter.
-# states <- unique(s$STATE)
-# 
-# attach(hm_st)
-# hm_s1_ind_mean <- hm_mean[status_group == "tracts_ind" & state == f[1]]
-# hm_s2_ind_mean <- hm_mean[status_group == "tracts_ind" & state == f[2]]
-# hme_s1_ind_mean <- hm_energy_mean[status_group == "tracts_ind" & state == f[1]]
-# hme_s2_ind_mean <- hm_energy_mean[status_group == "tracts_ind" & state == f[2]]
-# hm_s1_fam_mean <- hm_mean[status_group == "tracts_fam" & state == f[1]]
-# hm_s2_fam_mean <- hm_mean[status_group == "tracts_fam" & state == f[2]]
-# hme_s1_fam_mean <- hm_energy_mean[status_group == "tracts_fam" & state == f[1]]
-# hme_s2_fam_mean <- hm_energy_mean[status_group == "tracts_fam" & state == f[2]]
-# detach(hm_st)
-
-
-
-
-
-
+## --------------------------------------------------------------------------
 ## What prop of status grp tracts and what # of ppl/families have hm >avg?
-
-# Create empty vectors for dumping values
-stat_grp <- as.character()
-
-# Human mod - all
-prop_tracts_gt_hm_natl <- as.numeric()
-num_gt_hm_natl <- as.numeric()
-prop_tracts_gt_hm_st <- as.numeric()
-num_gt_hm_st <- as.numeric()
-
-# Human mod by energy
-prop_tracts_gt_hme_natl <- as.numeric()
-num_gt_hme_natl <- as.numeric()
-prop_tracts_gt_hme_st <- as.numeric()
-num_gt_hme_st <- as.numeric()
 
 
 # Which zone? Turn on/off
 # zone <- s_b1_df ; buffer <- b1_size
-zone <- s_b2_df ; (buffer <- b2_size)
-# zone <- s_b3_df ; buffer <- b3_size
-
-# Grab list of status groups
-stat_grps <- c(unique(hm_st$status_group))
-
-# test
-i <- 2
-boo <- s_b2_df %>% dplyr::select(AHY2E003, black, hm, hm_energy, prop_ttl_area)
+# zone <- s_b2_df ; (buffer <- b2_size)
+zone <- s_b3_df ; buffer <- b3_size
+# Eliminate any rows that have zero ppl else get NA rows when subsetting. 
+zone <- zone[zone$AHY2E001 > 0,]
 
 
+# Create empty vectors for dumping values
+stat_grp <- as.character()
+unit_type <- as.character()
+
+# Human mod (all & energy) - natl
+prop_tracts_gt_hm_natl <- as.numeric()
+num_gt_hm_natl <- as.numeric()
+prop_tracts_gt_hme_natl <- as.numeric()
+num_gt_hme_natl <- as.numeric()
+
+# Human mod (all & energy) - state
+prop_tracts_gt_hm_st <- as.numeric()
+num_gt_hm_st <- as.numeric()
+prop_tracts_gt_hme_st <- as.numeric()
+num_gt_hme_st <- as.numeric()
+
+# Grab list of status groups, nixing baseline grps (tracts_ind, tracts_fam)
+(stat_grps <- c(unique(hm_st$status_group)))
+(1:length(stat_grps))[-c(25,26)]
 
 ## LOOP ## -------------------------------------------------------
-for (i in 1:length(stat_grps)){ # Loop thru status grps
+# Loop through status grps but exclude baselines (tracts_ind, tracts_fam)
+for (i in (1:length(stat_grps))[-c(25,26)]){ 
+# for (i in 2){ # test black
+# for (i in 4){ # test non-white
+# for (i in 19){ # test non-white w children in pov
   # Assign status grp name
   grp <- stat_grps[i]
   
-  # Look up the corresponding col with # ppl for that status grp
+  # Look up the corresponding col with # ppl/fams for that status grp
   code1 <- match(grp, lu$status_grp) %>% lu$code1[.]
   code1_sign <- match(grp, lu$status_grp) %>% lu$code1_sign[.]
-  # ... maybe add in other relevant codes here or skip ... #
-  
+  code2 <- match(grp, lu$status_grp) %>% lu$code2[.]
+  code2_sign <- match(grp, lu$status_grp) %>% lu$code2_sign[.]
+  code3 <- match(grp, lu$status_grp) %>% lu$code3[.]
+  code3_sign <- match(grp, lu$status_grp) %>% lu$code3_sign[.]
+  code4 <- match(grp, lu$status_grp) %>% lu$code4[.]
+  code4_sign <- match(grp, lu$status_grp) %>% lu$code4_sign[.]
+  code5 <- match(grp, lu$status_grp) %>% lu$code5[.]
+  code5_sign <- match(grp, lu$status_grp) %>% lu$code5_sign[.]
+
   # Look up the unit -- ie # ppl or # families
   unit <- match(grp, lu$status_grp) %>% lu$unit[.]
   
-  # Find the appropriate baseline based on unit
+  # Find the appropriate baseline based on unit (individ or fam)
   hm_base <- hm_natl$hm_mean[hm_natl$status_group == unit]
   hme_base <- hm_natl$hm_energy_mean[hm_natl$status_group == unit]
   
-  # Compute ttl # tracts in status grp
-  ttl <- sum(zone[grp], na.rm = TRUE)
+  # Pull tracts in status grp w hm/hme > avg; row of NAs may be returned b/c of other NAs in df
+  all <- zone[zone[grp] == 1,] 
+  sel_hm <- all[all$hm > hm_base,]
+  sel_hme <- all[all$hm_energy > hme_base,]
   
-  # Compute prop tracts in status grp w hm or hme > natl avg
-  prop_hm <- sum(zone[grp][zone$hm > hm_base,], na.rm = TRUE) / sum(zone[grp], na.rm = TRUE) 
-  prop_hme <- sum(zone[grp][zone$hm_energy > hme_base,], na.rm = TRUE) / sum(zone[grp], na.rm = TRUE) 
+  # Compute prop of grp tracts w hm/hme > avg
+  prop_hm <- round(nrow(sel_hm)/nrow(all),4)
+  prop_hme <- round(nrow(sel_hme)/nrow(all),4)
   
+  # Compute ttl # ppl or fams w hm/hme > avg, correcting for partial tract areas.
+  # Several grps use multiple codes (additive is +1 in sign, subtract is -1)
+  # Deal with those special cases first...
+  if(grp %in% c("non_white", "no_ch", "nwht_lowin")) {
+    num_hm = round(sum(sel_hm[code1] * sel_hm$prop_ttl_area * code1_sign,
+                       sel_hm[code2] * sel_hm$prop_ttl_area * code2_sign))
+  } else if(grp %in% c("non_wht_ch", "nwht_chpov")){
+    num_hm = round(sum(sel_hm[code1] * sel_hm$prop_ttl_area * code1_sign,
+                       sel_hm[code2] * sel_hm$prop_ttl_area * code2_sign,
+                       sel_hm[code3] * sel_hm$prop_ttl_area * code3_sign,
+                       sel_hm[code4] * sel_hm$prop_ttl_area * code4_sign,
+                       sel_hm[code5] * sel_hm$prop_ttl_area * code5_sign))
+  } else {num_hm <- round(sum(sel_hm[code1] * sel_hm$prop_ttl_area * code1_sign))
+  }
   
+  if(grp %in% c("non_white", "no_ch", "nwht_lowin")) {
+    num_hme = round(sum(sel_hme[code1] * sel_hme$prop_ttl_area * code1_sign,
+                        sel_hme[code2] * sel_hme$prop_ttl_area * code2_sign))
+  } else if(grp %in% c("non_wht_ch", "nwht_chpov")){
+    num_hme = round(sum(sel_hme[code1] * sel_hme$prop_ttl_area * code1_sign,
+                       sel_hme[code2] * sel_hme$prop_ttl_area * code2_sign,
+                       sel_hme[code3] * sel_hme$prop_ttl_area * code3_sign,
+                       sel_hme[code4] * sel_hme$prop_ttl_area * code4_sign,
+                       sel_hme[code5] * sel_hme$prop_ttl_area * code5_sign))
+  } else {num_hme <- round(sum(sel_hme[code1] * sel_hme$prop_ttl_area * code1_sign))
+  }
   
+    # Run loop thru each state if necessary, combining vals for 2 states into 1
+    # for (i in 1:length(f)){ # Loop thru each state
+    # }
   
-  
-  
-  
-  
-  # compute ttl tracts in status grp
-  # select tracts in status grp > natl avg for hm
-  # compute proportion of ttl tracts and stick in prop_tracts_gt_hm_natl
-  # multiply selected pop col by tract weight (to address partial tracts)
-  # add pop and stick in num_ppl_gt_hm_natl
-  # select tracts in status grp > natl avg for hme
-  # compute proportion of ttl tracts and stick in prop_tracts_gt_hme_natl
-  # multiply selected pop col by tract weight (to address partial tracts)
-  # add pop and stick in num_ppl_gt_hme_natl
-  for (i in 1:length(f)){ # Loop thru each state
-    # Assign state name
-    # extract hm and hme for national
-    # extract hm and hme for selected state
-    # for status grp 
-  } 
-  # Combine vals for the 2 diff states before folding into 1
+stat_grp <- c(stat_grp, grp)
+unit_type <- c(unit_type, unit)
+prop_tracts_gt_hm_natl <- c(prop_tracts_gt_hm_natl, prop_hm)
+prop_tracts_gt_hme_natl <- c(prop_tracts_gt_hme_natl, prop_hme)
+num_gt_hm_natl <- c(num_gt_hm_natl, num_hm)
+num_gt_hme_natl <- c(num_gt_hme_natl, num_hme)
+
+print(paste0(grp, " tracts complete."))
 }
 
-  
-}  
-}
-hm_st$hm_mean[hm_st$status_group == "tracts_ind" & hm_st$state == f[1]]
+foo <- cbind(loc, buffer, stat_grp, unit_type,
+             prop_tracts_gt_hm_natl, prop_tracts_gt_hme_natl,
+             num_gt_hm_natl, num_gt_hme_natl) %>% as.data.frame()
+
+loc
+buffer
+write.csv(foo, paste0(out.dir,loc,"_",buffer,"_hm_stats_",today,".csv"))
 
 
 
-## What prop non-white tracts within 50 miles of Caster Range have > natl median hm?
-## What prop white tracts within 50 miles of Caster Range have > natl median hm?
-(ttl_tracts_hisp <- sum(txnm_50mi$hispanic, na.rm = TRUE))
-(ttl_tracts_wht_nohisp <- sum(txnm_50mi$wht_nohisp, na.rm = TRUE))
-
-(num_tracts_hisp_gt_natl_hm_med <- sum(txnm_50mi$hispanic[txnm_50mi$hm > hm_natl_med], na.rm = TRUE))
-(num_tracts_wht_nohisp_gt_natl_hm_med <- sum(txnm_50mi$wht_nohisp[txnm_50mi$hm > hm_natl_med], na.rm = TRUE))
-
-
-(num_tracts_hisp_gt_tx_hm_med <- sum(txnm_50mi$hispanic[txnm_50mi$STATE == "Texas" &
-                                                         txnm_50mi$hm > hm_tx_med], na.rm = TRUE))
-(num_tracts_hisp_gt_nm_hm_med <- sum(txnm_50mi$hispanic[txnm_50mi$STATE == "New Mexico" &
-                                                         txnm_50mi$hm > hm_nm_med], na.rm = TRUE))
-(num_tracts_hisp_gt_state_hm_med <- num_tracts_hisp_gt_tx_hm_med + num_tracts_hisp_gt_nm_hm_med)
 
 
 
-(num_tracts_wht_nohisp_gt_tx_hm_med <- sum(txnm_50mi$wht_nohisp[txnm_50mi$STATE == "Texas" &
-                                                                 txnm_50mi$hm > hm_tx_med], na.rm = TRUE))
-(num_tracts_wht_nohisp_gt_nm_hm_med <- sum(txnm_50mi$wht_nohisp[txnm_50mi$STATE == "New Mexico"
-                                                               & txnm_50mi$hm > hm_nm_med], na.rm = TRUE))
-(num_tracts_hisp_gt_state_hm_med <- num_tracts_wht_nohisp_gt_tx_hm_med + num_tracts_wht_nohisp_gt_nm_hm_med)
+## ------------------------------------------------------------------------------------------------
+# ## Testing grounds
+# stat_grps
+# # test w black
+# boo <- s_b2_df %>% dplyr::select(AHY2E003, black, hm, hm_energy, prop_ttl_area)
+# boo <- boo %>% filter(black == 1)
+# boo <- boo %>% filter(hm > hm_base)
+# boo$n <- boo$AHY2E003
+# boo$n <- round(boo$n * boo$prop_ttl_area)
+# sum(boo$n)
+# 
+# # test w non-white
+# boo <- s_b2_df %>% dplyr::select(AHY2E001, AHZAE003, non_white, hm, hm_energy, prop_ttl_area)
+# boo <- boo %>% filter(non_white == 1, hm > hm_base)
+# boo$n <- boo$AHY2E001 - boo$AHZAE003
+# boo$n <- round(boo$n * boo$prop_ttl_area)
+# sum(boo$n)
+# # test w nonwhite ch pov
+# boo <- s_b2_df %>% dplyr::select(AIF6CHPOV, AIF7CHPOV, AIF8CHPOV, AIOTHCHPOV, AIGDCHPOV, nwht_chpov, hm, hm_energy, prop_ttl_area)
+# boo <- boo %>% filter(nwht_chpov == 1)
+# boo <- boo %>% filter(hm > hm_base)
+# attach(boo)
+# boo$n <- round(AIF6CHPOV + AIF7CHPOV + AIF8CHPOV + AIOTHCHPOV + AIGDCHPOV)
+# detach(boo)
+# boo$n <- round(boo$n * boo$prop_ttl_area)
+# sum(boo$n) 
+# # test w low inc
+# boo <- s_b2_df %>% dplyr::select(AHY2E001, low_inc, hm, hm_energy, prop_ttl_area)
+# boo <- boo %>% filter(low_inc == 1)
+# boo <- boo %>% filter(hm > hm_base)
+# boo$n <- boo$AHY2E001
+# boo$n <- round(boo$n * boo$prop_ttl_area)
+# sum(boo$n)
 
 
-num_tracts_hisp_gt_natl_hm_med/ttl_tracts_hisp
-num_tracts_wht_nohisp_gt_natl_hm_med/ttl_tracts_wht_nohisp
+
+
